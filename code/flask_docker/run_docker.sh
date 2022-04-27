01f0a0736744064fb8218d925878a2538b637a2d
Bartek Szymanski <bartekszymanski@barteks-macbook-pro.home>
2022-04-27 12:44:17 +0200

    working flask app with docker container

---
+++ code/flask_docker/run_docker.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+docker image build -t flask_docker .
+docker run -p 5000:5000 -d flask_docker
---
+++ code/flask_docker/setup_frontend.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+echo ""
+echo "Skrypt instalacyjny do frontu z flaska"
+
+echo "Zostanie uruchomiona instalacja pakietów z pliku requirements.txt"
+echo ""
+pip install -r requirements.txt
+
+
+#EKSPERYMENTALNE, OBOWIAZKOWE PRZY INSTALACJI pip install flask
+echo "Uruchamiam eksport ustawien flask"
+export FLASK_APP=hello.py
+export FLASK_ENV=development
+
+echo "WERSJE ZAINSTALOWANE:"
+echo ""
+echo `flask --version`
+echo `docker --version`
+echo ""
