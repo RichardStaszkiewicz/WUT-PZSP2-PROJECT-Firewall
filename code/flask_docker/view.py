fe634bb6edd7fc79a43bd6080b60988d0ce989da
Bartek Szymanski <bartekszymanski@barteks-macbook-pro.home>
2022-04-27 12:44:17 +0200

    working flask app with docker container

--- code/flask_docker/view.py
+++ code/flask_docker/view.py
@@ -1,55 +1,17 @@
-import json
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template
 import os
 
-# mocks
-from rules import get_rules
-from logs import get_logs
-
-
 app = Flask(__name__)
-
-rules = get_rules()
-
-@app.route('/getRules', methods=['GET', 'POST'])
-def update_rules():
-    if request.method == 'POST':
-
-        if request.args.get('action') == 'disableAll':
-            for rule in rules:
-                rule['is_active'] = False
-            return 'ok'
-
-        if request.args.get('action') == 'enableAll':
-            for rule in rules:
-                rule['is_active'] = True
-            return 'ok'
-
-        if request.args.get('action') == 'save':
-            rules_data = request.get_json()
-            with open('code/Conf.json', 'w') as file:
-                json.dump(rules_data, file) 
-            return 'ok'
-
-        rule_data = request.get_json()
-        for i in range(len(rules)):
-            if rules[i]['id'] == rule_data['id']:
-                rules[i] = rule_data
-                return 'ok'
-
-        rules.append(rule_data)
-        return 'ok'
-
-    if request.method == 'GET':
-        return jsonify(rules)
 
 
 @app.route('/')
 def home():
-    logs = get_logs()
-    return render_template('index.html', logs=logs, rules=rules)
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+
+    
