d462c689362964c323688f9d3b19170e2d9637d1
Bartek Szymanski <bartekszymanski@Barteks-MacBook-Pro.local>
2022-04-29 20:24:11 +0200

    some trial and error

--- code/flask_docker/view.py
+++ code/flask_docker/view.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -7,11 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
+    
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text firewall']
+    processed_text = text.upper()
+    return processed_text
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
-
+    
 
     
