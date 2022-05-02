01195a37ec6dc099f7bd25ad22861a3b101cfa3d
Bartosz <bartek@Bartoszs-MacBook-Pro.local>
2022-05-02 12:52:09 +0200

    created add rule modal, moved logs to separate file

--- code/flask_docker/rules.py
+++ code/flask_docker/rules.py
@@ -1,6 +1,22 @@
-import json
+rules = [
+    {
+        "id": 1, "name": "ASD", "protocol": 1, "profile": 0,
+        "direction": 1, "analysed_param": 0, "expected_val": 0
+    },
+    {
+        "id": 4, "name": "SDASD", "protocol": 2, "profile": 0,
+        "direction": 0, "analysed_param": 0, "expected_val": 0
+    },
+    {
+        "id": 5, "name": "FW", "protocol": 1, "profile": 3,
+        "direction": 0, "analysed_param": 5, "expected_val": "f2"
+    },
+    {
+        "id": 5, "name": "FW", "protocol": 1, "profile": 3,
+        "direction": 0, "analysed_param": 5, "expected_val": "f2"
+    },
+]
+
 
 def get_rules():
-    with open('code/Conf.json', 'r') as file:
-        rules = json.load(file)
     return rules
--- code/flask_docker/static/js/addRule.js
+++ code/flask_docker/static/js/addRule.js
@@ -19,68 +19,25 @@ window.onclick = (event) => {
 }
 
 //inputs 
+const ruleName = document.getElementById('ruleName').value;
+const ruleProtocol = document.getElementById('ruleProtocol').value;
+const ruleProfile = document.getElementById('ruleProfile').value;
+const ruleDirection = document.getElementById('ruleDirection').value;
+const ruleParameters = document.getElementById('ruleParameters').value;
+const ruleExpectedValue = document.getElementById('ruleExpectedValue').value;
+
 const addRuleBtn = document.getElementById('confirmAddRule');
 
+const rule = {
+    name: ruleName,
+    protocol: ruleProtocol,
+    profile: ruleProfile,
+    direction: ruleDirection,
+    analysed_param: ruleParameters,
+    expected_val: ruleExpectedValue,
+}
+
 addRuleBtn.onclick = () => {
-    const ruleName = document.getElementById('rule_name').value;
-    const ruleProtocol = document.getElementById('rule_protocol').value;
-
-    const rule = {
-        id: RULES.length + 1,
-        name: ruleName,
-        protocol: ruleProtocol,
-        is_active: true,
-    }
-
-    if(ruleProtocol === "IP/TCP") {
-        rule['source_address'] = document.getElementById('rule_source_address').value;
-        rule['destination_address'] = document.getElementById('rule_destination_address').value;
-        rule['source_port'] = document.getElementById('rule_source_port').value;
-        rule['destination_port'] = document.getElementById('rule_destination_port').value;
-    }
-
-    if(ruleProtocol === "MODBUS") {
-        rule['function'] = document.getElementById('rule_function').value;
-
-        if(rule.function === "f1") {
-            rule['starting_address'] = document.getElementById('rule_starting_address').value;
-            rule['last_address'] = document.getElementById('rule_last_address').value;
-        }
-        
-        if(rule.function === "f5") {
-            rule['output_address'] = document.getElementById('rule_output_address').value;
-            rule['value'] = document.getElementById('rule_value').value;
-        }
-
-        if(rule.function === "f6") {
-            rule['output_address'] = document.getElementById('rule_output_address').value;
-            rule['min_value'] = document.getElementById('rule_min_value').value;
-            rule['max_value'] = document.getElementById('rule_max_value').value;
-        }
-
-        if(rule.function === "f23") {
-            rule['read_starting_address'] = document.getElementById('rule_read_starting_address').value;
-            rule['read_last_address'] = document.getElementById('rule_read_last_address').value;
-            rule['write_starting_address'] = document.getElementById('rule_write_starting_address').value;
-            rule['write_last_address'] = document.getElementById('rule_write_last_address').value;
-        }
-    }
-
-    if(ruleProtocol === "SLMP") {
-        rule['command'] = document.getElementById('rule_command').value;
-        rule["subcommand"] = document.getElementById('rule_subcommand').value;
-        rule['min_value'] = document.getElementById('rule_min_value').value;
-        rule['max_value'] = document.getElementById('rule_max_value').value;
-    }
-
-    RULES.push(rule);
-
-    fetch('/getRules', {
-        method: 'POST',
-        headers: {
-            "Content-Type": 'application/json',
-        },
-        body: JSON.stringify(rule),
-    });
+    //TODO: create addRule function
 }
 
