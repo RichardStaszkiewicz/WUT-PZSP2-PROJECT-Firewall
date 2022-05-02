efaec0c4ecd34c731012f647eeaeaae2fdd55b16
Bartosz <bartek@Bartoszs-MacBook-Pro.local>
2022-05-02 09:57:26 +0200

    refactored rules

--- code/flask_docker/view.py
+++ code/flask_docker/view.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template, request
 import os
 
+# mocks
+from static.mocks.rules import getRules
+
+
 app = Flask(__name__)
+
+
+@app.route('/')
+def rules():
+    rules = getRules()
+    return render_template('rules.html', messages=rules)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
-    
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text firewall']
-    processed_text = text.upper()
-    return processed_text
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
-    
-
-    
