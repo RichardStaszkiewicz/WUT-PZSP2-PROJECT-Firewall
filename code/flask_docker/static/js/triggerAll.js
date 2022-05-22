154985b6be3399d2499cbbbdb4814dd2f0e28013
bkosinsk <01158749@pw.edu.pl>
2022-05-22 15:24:35 +0200

    changed rules import, rule add modal and change form

--- code/flask_docker/static/js/triggerAll.js
+++ code/flask_docker/static/js/triggerAll.js
@@ -19,11 +19,58 @@ const disableAll = () => {
 }
 
 const savePush = () => {
+    const rulesHTML = document.getElementById('rules').getElementsByTagName('li');
+    console.log(rulesHTML)
+
+    const ruules = rulesHTML.map(rule => {
+        console.log(rule)
+        return {
+            id: ''
+        }
+    })
+
+    const rules = [
+        {
+            "id": 101,
+            "name": "Wszystkie multiple read readujące mniej niz 100",
+            "direction": "IN",
+            "protocol": "MODBUS",
+            "command": "Device Read Multiple Registers",
+            "Start Register": "Any",
+            "quantity": 100,
+            "comparison": "MAX",
+            "is_active": true
+        },
+        {
+            "id": 102,
+            "name": "Wszystkie multiple read readujące mniej niz 150",
+            "direction": "IN",
+            "protocol": "MODBUS",
+            "command": "Device Read Multiple Registers",
+            "Start Register": "Any",
+            "quantity": 150,
+            "comparison": "MAX",
+            "is_active": true
+        },
+        {
+            "id": 103,
+            "name": "Wszystkie multiple read readujące mniej niz 170",
+            "direction": "IN",
+            "protocol": "MODBUS",
+            "command": "Device Read Multiple Registers",
+            "Start Register": "Any",
+            "quantity": 170,
+            "comparison": "MAX",
+            "is_active": true
+        }
+    ]
+
     fetch('/getRules?action=save', {
         method: 'POST',
         headers: {
             "Content-Type": 'application/json',
         },
+        body: JSON.stringify(rules)
     })
-    window.location.reload(true);
+    //window.location.reload(true);
 }
