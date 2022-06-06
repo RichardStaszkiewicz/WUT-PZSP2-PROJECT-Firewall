e5c17124e2f2f07e5947e239293e629d8a83f5e3
bkosinsk <01158749@pw.edu.pl>
2022-06-06 14:19:50 +0200

    added login page

--- code/flask_docker/view.py
+++ code/flask_docker/view.py
@@ -1,19 +1,24 @@
 import json
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import os
 import subprocess
 
-# mocks
 from rules import get_rules
 from logs import get_logs
 
 
 app = Flask(__name__)
 
-rules = get_rules()
+RULES = get_rules()
+LOGS = get_logs()
+IS_LOGIN = False
+USERNAME = 'admin'
+PASSWORD = 'admin'
 
 @app.route('/getRules', methods=['GET', 'POST'])
 def update_rules():
+    global LOGS
+
     if request.method == 'POST':
 
         if request.args.get('action') == 'disableAll':
@@ -24,6 +29,10 @@ def update_rules():
         if request.args.get('action') == 'enableAll':
             for rule in rules:
                 rule['is_active'] = True
+            return 'ok'
+
+        if request.args.get('action') == "fetchLogs":
+            LOGS = get_logs()
             return 'ok'
 
         if request.args.get('action') == 'save':
@@ -39,23 +48,36 @@ def update_rules():
 
         rule_data = request.get_json()
 
-        for i in range(len(rules)):
-            if rules[i]['id'] == rule_data['id']:
-                rules[i] = rule_data
+        for i in range(len(RULES)):
+            if RULES[i]['id'] == rule_data['id']:
+                RULES[i] = rule_data
                 return 'ok'
         
-        rules.append(rule_data)
+        RULES.append(rule_data)
         return 'ok'
 
     if request.method == 'GET':
-        return jsonify(rules)
+        return jsonify(RULES)
 
 
-@app.route('/')
+@app.route('/home')
 def home():
-    logs = get_logs()
-    return render_template('index.html', logs=logs, rules=rules)
+    if not IS_LOGIN:
+        return redirect(url_for('login'))
+    return render_template('index.html', logs=LOGS, rules=RULES)
 
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    global IS_LOGIN
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != USERNAME or request.form['password'] != PASSWORD:
+            error = 'Invalid Credentials. Please try again.'
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            IS_LOGIN = True
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
