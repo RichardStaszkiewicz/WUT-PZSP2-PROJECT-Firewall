eda5fa271502e7402419561d4142ae9cdb034a05
Bartlomiej Szymanski <bartlomiej.szymanski4.stud@pw.edu.pl>
2022-07-07 10:13:47 +0200

    working salting both in register and password checking

--- code/Hash/Hash.py
+++ code/Hash/Hash.py
@@ -1,42 +1,47 @@
 import hashlib
-import json 
+import json
 import os
-PASSWD_FILE = "./data/passwd.json"
+import random
+import string
 
-
+SALT_LEN = 8
 
 def get_user(login, filepath):
-    with open(filepath,"r") as passwd_file:
+    with open(filepath, "r") as passwd_file:
         data = json.load(passwd_file)
         for user in data["users"]:
             if user["login"] == login:
                 return user
+        return {}
 
-def is_password_correct(login,password, filepath): 
-    user = get_user(login,filepath)
-    salted_password = password+user['salt']
+
+def is_password_correct(login, password, filepath):
+    user = get_user(login, filepath)
+
     h = hashlib.sha384()
-    h.update(salted_password.encode())
+    h.update(password.encode())
+    h.update(user['salt'].encode())
+
     return h.hexdigest() == user['password']
+
 
 def register_user(login, password, filepath):
     data = {}
-    with open(filepath,"r") as passwd_file:
+    with open(filepath, "r") as passwd_file:
         data = json.load(passwd_file)
-    
+
     for user in data['users']:
         if user['login'] == login:
             return False
 
-    salt = os.urandom(8)
-    
+    salt = ''.join(random.choices(string.ascii_uppercase + string.digits, k=SALT_LEN))
     h = hashlib.sha384()
     h.update(password.encode())
-    h.update(salt)
+    h.update(salt.encode())
 
-    data['users'].append({'login' : login, 'password' : h.hexdigest(), 'salt' : str(salt) })   
-    
-    with open(filepath,"w") as passwd_file:
+    data['users'].append(
+        {'login': login, 'password': h.hexdigest(), 'salt': salt})
+
+    with open(filepath, "w") as passwd_file:
         json.dump(data, passwd_file)
     return True
-
