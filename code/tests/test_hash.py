3513e16e3b14e4082707befa77b896535320b138
Bartlomiej Szymanski <bartlomiej.szymanski4.stud@pw.edu.pl>
2022-07-07 10:10:29 +0200

    fixed writing to file, now all users are shown

--- code/tests/test_hash.py
+++ code/tests/test_hash.py
@@ -4,6 +4,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Hash'))
 import Hash
 import json
 import hashlib
+import pdb 
+
 
 JSON_FILE = 'code/tests/passwd_temp.json'
 
@@ -18,20 +20,18 @@ sample_json = {
 }
 
 
-def initialize_test_file():
+def initialize_test_file(input_data):
     with open(JSON_FILE, 'w') as outfile:
-            json.dump(sample_json, outfile)
+            json.dump(input_data, outfile)
 
 
 class TestHash(unittest.TestCase):
 
     def test_get_user(self):
-        initialize_test_file()
+        initialize_test_file(sample_json)
         expected_user = sample_json['users'][0]
         result_user = Hash.get_user('admin',JSON_FILE)
         self.assertEqual(expected_user,result_user)
-        
-
 
     def test_get_user2(self):
         test_password = 'nimda'
@@ -48,17 +48,15 @@ class TestHash(unittest.TestCase):
     def test_password_correct(self):
         self.assertTrue(Hash.is_password_correct('admin', 'nimda', JSON_FILE))
 
-    # def test_register_user_rep(self):
-    #     os.remove(JSON_FILE)
-    #     initialize_test_file()
+    def test_register_user_rep(self):
+        # initialize_test_file(sample_json)
 
-    #     self.assertFalse(Hash.register_user('admin', 'password', JSON_FILE))
+        self.assertFalse(Hash.register_user('admin', 'password', JSON_FILE))
 
        
     def test_register_user(self):
         data = {}
-        os.remove(JSON_FILE)
-        initialize_test_file()
+        # initialize_test_file(sample_json)
         Hash.register_user("Balbinka", "balbinkahaslo", JSON_FILE)
 
         with open(JSON_FILE,"r") as passwd_file:
